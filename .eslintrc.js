9b7cf4a3e5938472ef259ecb64aa8e78d732ca9f
kevin.sjj <kevin.sjj@kakaoent.com>
2022-07-09 17:57:42 +0900

    프로젝트 초기 설정

---
+++ .eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "plugin:storybook/recommended",
+  ],
+  plugins: ["react", "react-hooks", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "func-names": 0,
+    "no-undef": 0,
+    "no-plusplus": 0,
+    "no-console": 0,
+    "no-use-before-define": 0,
+    "no-restricted-syntax": 0,
+    "no-unused-vars": 0,
+    "react/react-in-jsx-scope": 0,
+    "react/prefer-stateless-function": 0,
+    "react/jsx-filename-extension": 0,
+    "react/jsx-one-expression-per-line": 0,
+    "react/function-component-definition": 0,
+    "react/jsx-props-no-spreading": 0,
+    "import/no-extraneous-dependencies": 0,
+    "import/no-unresolved": 0,
+    "import/extensions": 0,
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+  },
+  overrides: [
+    {
+      files: ["**/*.ts", "**/*.tsx"],
+      excludedFiles: ["*.test.ts", "*.test.tsx"],
+    },
+  ],
+  ignorePatterns: ["node_modules/**", "dist/**", "*.css", "*.scss"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
---
+++ .storybook/main.js
@@ -0,0 +1,29 @@
+module.exports = {
+  stories: [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    {
+      name: "storybook-addon-turbo-build",
+      options: {
+        optimizationLevel: 3,
+        disableSourceMap: true,
+      },
+    },
+  ],
+  framework: "@storybook/react",
+  features: {
+    storyStoreV7: true,
+  },
+  core: {
+    builder: "@storybook/builder-webpack5",
+    options: {
+      lazyCompilation: true,
+      fsCache: true,
+    },
+  },
+};
